6e9d4eb07846b64792fa07ea23e9b70a2a6a7d81
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-04-03 19:15:50 +0530

    first commit

---
+++ product-service/src/test/java/lk/ijse/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
